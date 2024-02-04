283f8c0d9a5d99bb1d3232b596583838f870e691
bishalbera <bishalbera2@gmail.com>
2024-02-04 16:23:35 +0530

    added map handler func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bishalbera/url-shortener
+
+go 1.21.6
---
+++ handler.go
@@ -0,0 +1,24 @@
+package urlshortener
+
+import "net/http"
+
+
+/*
+MapHandler will return an http.HandlerFunc (which also implements http.handler) 
+that will attempt to map any paths(keys in the map) to their corresponding URL 
+(values that each key in the map points to, in the string format). If the path
+is not provided in the map, then the fallback http.Handler will be called
+instead.
+*/
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		path:= r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w,r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w,r)
+	}
+
+}
